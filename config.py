ecb9dac10bcdae30fdf7a17e007886cf83c36e0c
Hamnah Durrani <hamnahdurrani1026@gmail.com>
2026-03-02 15:55:52 -0500

    Updated NPJWI_BASE_URL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config(BaseSettings):
     npjwi_timeout: int = 25
     npjwi_base_url: str = "/evaluation"
     env_routing: dict = {
-        "dev": "http://192.168.123.82:9000",
+        "dev": "https://npka-npjwi-dev.inago.com/nextgen",
     }
  
 config = Config()
